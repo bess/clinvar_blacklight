476aa90d7c2bc1d6f9fd6653fe6f43f69de856ec
Bess Sadler <bess@stanford.edu>
2016-11-22 17:06:23 -0800

    Parse some data

--- lib/clin_var_parser.rb
+++ lib/clin_var_parser.rb
@@ -1,8 +1,10 @@
 require 'nokogiri'
+require 'clin_var_set'
 
 class ClinVarParser
 
   attr_reader :clinvar_xml_file
+  attr_reader :clin_var_set
 
   def initialize(clinvar_xml_file)
     @clinvar_xml_file = clinvar_xml_file
@@ -12,8 +14,15 @@ class ClinVarParser
     puts "parsing... "
     reader = Nokogiri::XML::Reader(File.open(@clinvar_xml_file, "r"))
     while reader.read
-      if reader.name == 'Title' && reader.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
-        puts reader.inner_xml.inspect # TODO xml decode this, if necessary.
+
+      # When we encounter a ClinVarSet, make a new ClinVarSet object and populate it
+      if reader.name == 'ClinVarSet' && reader.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+        @clin_var_set = ClinVarSet.new(reader.attribute('ID'))
+        @clin_var_set.record_status = reader.read.read.read.value
+        @clin_var_set.replaces = reader.read.read.read.value
+        @clin_var_set.title = reader.read.read.read.value
+
+        # puts reader.inner_xml.inspect # TODO xml decode this, if necessary.
       end
     end
   end
--- lib/clin_var_set.rb
+++ lib/clin_var_set.rb
@@ -1,5 +1,7 @@
 class ClinVarSet
   attr_accessor :id
+  attr_accessor :record_status
+  attr_accessor :replaces
   attr_accessor :title
 
   def initialize(id)
